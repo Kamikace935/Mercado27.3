a4a8422877110fc479a8ab96a85e036157dee171
Pablo Marián <pablomarianv@gmail.com>
2023-04-19 16:11:38 +0200

    Modificación estructura ficheros

--- js/Cliente.js
+++ js/Cliente.js
@@ -1,36 +1,3 @@
-function guardarProducto() {
-
-    const codigo = document.getElementById("codigo").value;
-    const descripcion = document.getElementById("descripcion").value;
-    const importe = document.getElementById("importe").value;
-    const stock = document.getElementById("stock").value;
-
-// valida que se hayan ingresado los campos obligatorios
-    if (!codigo || !descripcion || !importe || !stock) {
-        alert("Por favor, complete todos los campos");
-        return;
-    }
-
-// crea un objeto con los datos del producto
-    const producto = {
-        codigo,
-        descripcion,
-        importe,
-        stock
-    };
-
-// agrega el producto a la lista de productos
-    const listaProductos = JSON.parse(localStorage.getItem("productos")) || [];
-    listaProductos.push(producto);
-    localStorage.setItem("productos", JSON.stringify(listaProductos));
-
-// limpia los campos del formulario
-    document.getElementById("codigo").value = "";
-    document.getElementById("descripcion").value = "";
-    document.getElementById("importe").value = "";
-    document.getElementById("stock").value = "";
-}
-
 const buscador = document.getElementById("buscador")
 buscador.addEventListener("input", buscarProducto)
 
---
+++ js/Empleado.js
@@ -0,0 +1,32 @@
+function guardarProducto() {
+
+    const codigo = document.getElementById("codigo").value;
+    const descripcion = document.getElementById("descripcion").value;
+    const importe = document.getElementById("importe").value;
+    const stock = document.getElementById("stock").value;
+
+//  Valida que se hayan ingresado los campos obligatorios
+    if (!codigo || !descripcion || !importe || !stock) {
+        alert("Por favor, complete todos los campos");
+        return;
+    }
+
+//  Crea un objeto con los datos del producto
+    const producto = {
+        codigo,
+        descripcion,
+        importe,
+        stock
+    };
+
+//  Agrega el producto a la lista de productos
+    const listaProductos = JSON.parse(localStorage.getItem("productos")) || [];
+    listaProductos.push(producto);
+    localStorage.setItem("productos", JSON.stringify(listaProductos));
+
+//  Limpia los campos del formulario
+    document.getElementById("codigo").value = "";
+    document.getElementById("descripcion").value = "";
+    document.getElementById("importe").value = "";
+    document.getElementById("stock").value = "";
+}
